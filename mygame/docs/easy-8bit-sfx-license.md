7e41ab098734105916e20ff68a8a796c20b407f2
ayanakamu34-wq <ayanakamu34@gmail.com>
2026-09-03 16:24:06 +0900

    効果音およびライセンス

---
+++ mygame/docs/easy-8bit-sfx-license.md
@@ -0,0 +1,5 @@
+# EASY 8BIT SFXについて
+ゲーム中のBGMについては、EASY 8BIT SFX様で生成した音源を使用しています。
+
+- 制作会社：AMIX
+- サイト：https://amix-design.com/tl/tool-s-8bit-sfx/about.html
--- mygame/source/sound.c
+++ mygame/source/sound.c
@@ -1,6 +1,8 @@
 #include <maxmod9.h>
 #include <nds.h>
 
+#include "game_types.h"
+#include "sound.h"
 #include "soundbank.h"
 #include "soundbank_bin.h"
 
@@ -26,8 +28,42 @@ static void bgmInit() {
   playBGM();
 }
 
+static void soundEffectInit() {
+  /*勝敗決定時に鳴らす*/
+  mmLoadEffect(SFX_SFX_FANFARE_20260903_1442);
+  /*攻撃相手確定時に鳴らす*/
+
+  /*その他決定時に鳴らす*/
+  mmLoadEffect(SFX_SFX_SELECT_20260902_2133);
+}
+
+bool hpDecreased(Game *game, Game *preGame) {
+  int i;
+  for (i = 0; i < UNIT_COUNT; i++) {
+    if (game->units[i].hp != preGame->units[i].hp) {
+      return true;
+    }
+  }
+  return false;
+}
+void playAppropriateSE(Game *game, Game *preGame) {
+  /*gameとpreGameがNULLじゃないか判定*/
+  if (game != NULL && preGame != NULL) {
+    /*phaseが他の何かからPHASE_GAME_OVERに == ゲームが終わった瞬間*/
+    if (preGame->phase != PHASE_GAME_OVER && game->phase == PHASE_GAME_OVER)
+      mmEffectVolume(mmEffect(SFX_SFX_FANFARE_20260903_1442), 255);
+    /*敵のHPが変動 == 攻撃成立*/
+    /*else if (hpDecreased(game, preGame))
+      mmEffect(SFX_SFX_SWORD_20260902_2131);*/
+    /*フェーズが変動 == Aで有効な状態遷移*/
+    else if (preGame->phase != game->phase)
+      mmEffect(SFX_SFX_SELECT_20260902_2133);
+  }
+}
+
 /*main関数実行開始時の一回のみ呼び出す。*/
 void soundInit() {
   setUpPlayer();
   bgmInit();
+  soundEffectInit();
 }
