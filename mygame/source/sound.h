68c9436cf26e1a270a443af0eda123f2d0f92e49
ayanakamu34-wq <ayanakamu34@gmail.com>
2026-09-03 12:40:48 +0900

    BGMの再生機能を実装

---
+++ mygame/source/sound.h
@@ -0,0 +1,14 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include "game_types.h"
+void playBGM();
+
+void stopBGM();
+
+/*BGMの再生状況をリセットする。リセットするだけで流しなおしてはくれないので、流すときにplayBGM()を呼び出す必要あり*/
+void resetBGM();
+
+/*main関数実行開始時の一回のみ呼び出す。*/
+void soundInit();
+#endif /*SOUND_H*/
